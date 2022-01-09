4f7a948663c58a09b54d77e906a87c7682d35023
spider-luis <marcux2001@gmail.com>
2022-01-09 18:00:27 -0500

    add key into prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'https://gateway.marvel.com/v1/public/',
+  marvelKeys: {
+    publicKey: '5a237863b3cc2061003cbbc4fe20dc06',
+    privateKey: 'fbf255068eccea6d0ef951b9f25626b57ab2fe72',
+  },
 };
